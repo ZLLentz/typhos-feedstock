12a1499e99c7ca74716dc84a65a6eb787a00e9c2
Zachary Lentz <zlentz@slac.stanford.edu>
2020-03-26 11:45:53 -0700

    MNT: follow shellcheck's suggestions

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,21 +1,21 @@
 # Install the package
-python -m pip install . --no-deps -vv
+"${PYTHON}" -m pip install . --no-deps -vv
 
 # Create auxillary
-mkdir -p $PREFIX/etc/conda/activate.d
-mkdir -p $PREFIX/etc/conda/deactivate.d
-mkdir -p $PREFIX/etc/typhos
+mkdir -p "${PREFIX}/etc/conda/activate.d"
+mkdir -p "${PREFIX}/etc/conda/deactivate.d"
+mkdir -p "${PREFIX}/etc/typhos"
 
 # Create auxiliary vars
-DESIGNER_PLUGIN_PATH=$PREFIX/etc/typhos
-DESIGNER_PLUGIN=$DESIGNER_PLUGIN_PATH/typhos_designer_plugin.py
-ACTIVATE=$PREFIX/etc/conda/activate.d/typhos.sh
-DEACTIVATE=$PREFIX/etc/conda/deactivate.d/typhos.sh
+DESIGNER_PLUGIN_PATH="${PREFIX}/etc/typhos"
+DESIGNER_PLUGIN="${DESIGNER_PLUGIN_PATH}/typhos_designer_plugin.py"
+ACTIVATE="${PREFIX}/etc/conda/activate.d/typhos.sh"
+DEACTIVATE="${PREFIX}/etc/conda/deactivate.d/typhos.sh"
 
-echo "from typhos.designer import *" >> $DESIGNER_PLUGIN
-echo "export PYQTDESIGNERPATH="$DESIGNER_PLUGIN_PATH":\$PYQTDESIGNERPATH" >> $ACTIVATE
-echo "export PYDM_DESIGNER_ONLINE=True" >> $ACTIVATE
-echo "unset PYQTDESIGNERPATH" >> $DEACTIVATE
+echo "from typhos.designer import *" >> "${DESIGNER_PLUGIN}"
+echo "export PYQTDESIGNERPATH=\"${DESIGNER_PLUGIN_PATH}\":\"\${PYQTDESIGNERPATH}\"" >> "${ACTIVATE}"
+echo "export PYDM_DESIGNER_ONLINE=True" >> "${ACTIVATE}"
+echo "unset PYQTDESIGNERPATH" >> "${DEACTIVATE}"
 
 unset DESIGNER_PLUGIN_PATH
 unset DESIGNER_PLUGIN
